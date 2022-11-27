65d238ca9e17afda2062e1a87ee605cab56305cc
Primo Acho <primo.acho@BOL-LT-210913G.local>
2022-11-27 16:13:40 -0400

    menu effect fixed

--- src/components/HeaderMenu/Header.jsx
+++ src/components/HeaderMenu/Header.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef, Fragment} from "react";
 import { slide as Menu } from "react-burger-menu";
 import './sideMenu.css';
 
@@ -36,8 +36,7 @@ const Header = () => {
         setOpen(false);
     }
     return (
-        <nav ref={navElem} className="bg-white h-14 shadow-md flex flex-row items-center justify-between fixed w-screen max-w-[100%] z-20">
-            {open && 
+        <Fragment>
             <Menu
                 isOpen={open} 
             >
@@ -47,30 +46,31 @@ const Header = () => {
                     <a href={elem.url} onClick={handleClose}>{elem.name}</a>
                 )
             })}
-
-        </Menu>}
-           <button onClick={handleOpen} className="px-4 md:hidden"><i className="fa-solid fa-bars"></i></button>
-           <div className="px-2 flex flex-row items-center">
-            <h2>Juegos Bautistas El Alto 2023</h2>
-            <img src="/Olympic_rings.svg" className="w-20 h-14 px-2"/>
-           </div>
-           <div className="hidden md:block">
-             <ul className="flex flex-row items-center">
-                <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
-                    Inicio
-                </li>
-                <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
-                    Disciplinas
-                </li>
-                <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
-                    Inscripciones
-                </li>
-                <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
-                    Resultados
-                </li>
-             </ul>
-           </div>
-        </nav>
+            </Menu>
+            <nav ref={navElem} className="bg-white h-14 shadow-md flex flex-row items-center justify-between fixed w-screen max-w-[100%] z-20">
+                <button onClick={handleOpen} className="px-4"><i className="fa-solid fa-bars"></i></button>
+                <div className="px-2 flex flex-row items-center">
+                    <h2>Juegos Bautistas El Alto 2023</h2>
+                    <img src="/Olympic_rings.svg" className="w-20 h-14 px-2"/>
+                </div>
+                <div className="hidden md:block">
+                    <ul className="flex flex-row items-center">
+                        <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
+                            Inicio
+                        </li>
+                        <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
+                            Disciplinas
+                        </li>
+                        <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
+                            Inscripciones
+                        </li>
+                        <li className="px-4 hover:bg-gray-900 hover:text-white hover:rounded-md hover:h-8 flex items-center">
+                            Resultados
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </Fragment>
     )
 };
 export default Header;
